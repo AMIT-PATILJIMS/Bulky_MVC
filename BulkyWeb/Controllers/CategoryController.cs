e8e3fa1101b7778cfbdb24650537c094ceb03e59
AMIT-PATILJIMS <ap1079997@gmail.com>
2023-08-27 10:57:20 +0530

    Changes: 1. Pass the data in View() method. 2. Use for loop in view Pages.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
@@ -12,8 +13,8 @@ namespace BulkyWeb.Controllers
         }
         public IActionResult Index()
         {
-            var objData = _db.Categories.ToList();
-            return View();
+            List<Category> objData = _db.Categories.ToList();
+            return View(objData);
         }
     }
 }
