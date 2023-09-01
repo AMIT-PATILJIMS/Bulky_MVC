0c995e0e16cb058af93cf9160facecdb903b4fe7
AMIT-PATILJIMS <ap1079997@gmail.com>
2023-09-01 08:44:59 +0530

    Changes: 1. Do some changes in Razor project.

---
+++ BulkyWeb_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyWeb_Temp.Models;
+
+namespace BulkyWeb_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", Displayorder = 1 },
+                new Category { Id = 2, Name = "Laddu", Displayorder = 2 },
+                new Category { Id = 3, Name = "Gopal", Displayorder = 3 }
+                );
+        }
+    }
+}
